19ead44ef7b03cd6fc482d9ffe34f49cce318029
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-05-26 12:37:14 +0800

    create slick carousel feature

---
+++ app/containers/animated-flatlist/data.js
@@ -0,0 +1,5 @@
+// https://www.flaticon.com/packs/retro-wave
+// inspiration: https://dribbble.com/shots/11164698-Onboarding-screens-animation
+// https://twitter.com/mironcatalin/status/1321180191935373312
+
+//  export default
